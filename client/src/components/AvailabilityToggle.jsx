7eb97de35dcfba218eba40f3fce663387e1ea155
Stranger-Siv <programmersiv21@gmail.com>
2026-02-04 15:49:20 +0530

    changes

--- client/src/components/AvailabilityToggle.jsx
+++ client/src/components/AvailabilityToggle.jsx
@@ -90,10 +90,10 @@ function AvailabilityToggle() {
 
         {/* Tooltip - desktop only (no hover popover on small screens) */}
         {userMode === 'worker' && !isOnline && (
-          <div className="absolute left-0 top-full mt-2 hidden md:group-hover:block z-50">
-            <div className="relative bg-gray-900 dark:bg-gray-800 text-white dark:text-gray-100 text-xs rounded-lg px-3 py-2 shadow-lg w-64 whitespace-nowrap text-left border border-gray-700">
+          <div className="absolute left-1/2 -translate-x-1/2 top-full mt-2 hidden md:group-hover:block z-50">
+            <div className="relative inline-block bg-gray-900 dark:bg-gray-800 text-white dark:text-gray-100 text-xs rounded-lg px-3 py-2 shadow-lg whitespace-nowrap text-center border border-gray-700">
               Go online to receive nearby task alerts
-              <div className="absolute -top-1 left-4 w-2 h-2 bg-gray-900 dark:bg-gray-800 transform rotate-45 border-l border-t border-gray-700" />
+              <div className="absolute -top-1 left-1/2 -translate-x-1/2 w-2 h-2 bg-gray-900 dark:bg-gray-800 transform rotate-45 border-l border-t border-gray-700" />
             </div>
           </div>
         )}
